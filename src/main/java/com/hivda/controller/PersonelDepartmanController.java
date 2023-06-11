9cb3e210c4d6b1fb8cee146da04dba2a971d56dd
HivdaAydogan <hivdaydgn@gmail.com>
2023-06-11 13:21:39 +0300

    Exceptions

--- src/main/java/com/hivda/controller/PersonelDepartmanController.java
+++ src/main/java/com/hivda/controller/PersonelDepartmanController.java
@@ -1,6 +1,8 @@
 package com.hivda.controller;
 
 import com.hivda.dto.request.GetNameUpperCaseRequestDto;
+import com.hivda.exceptions.ErrorType;
+import com.hivda.exceptions.Java8StartException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,20 @@ public class PersonelDepartmanController {
 
     @PostMapping("/getname")
     public ResponseEntity<String> getNameUpperCase(@RequestBody GetNameUpperCaseRequestDto dto){
+        if (dto.getName()==null)
+            throw new Java8StartException(ErrorType.NAME_IS_NULL);
         String nameUpper = dto.getName().toUpperCase();
         return ResponseEntity.ok(nameUpper);
     }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/hivda/exceptions/Java8StartException.java
+++ src/main/java/com/hivda/exceptions/Java8StartException.java
@@ -1,4 +1,23 @@
 package com.hivda.exceptions;
 
-public class Java8StartException {
+import lombok.Getter;
+
+/**
+ * Bu kısımda uygulamamız içinde oluşabilecek tüm hataları kapsayan bir kapsayıcıya ihtiyacımız var,
+ * yani hata tiplerini listesini içeren bir Enum sınıfı oluşturabiliriz.
+ * Ayrıca, uygulamamız restApi kurgusunda olduğu için bu hataların Response Entity oluşturmak doğru yaklaşımdır.
+ */
+@Getter
+public class Java8StartException extends RuntimeException{
+    private final ErrorType errorType;
+    public Java8StartException(ErrorType errorType){
+        super(errorType.getMessage());
+        this.errorType = errorType;
+    }
+
+    public Java8StartException(ErrorType errorType, String message){
+        super(message);
+        this.errorType = errorType;
+    }
+
 }
